cfd02e545aa7e121ebabf00d1388c2382b13176a
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-11-05 16:52:48 +0900

    Node.js crash course: set first practice

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.setHeader('Content-type', 'text/html');
+
+  let path = './views/';
+  switch (req.url){
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default :
+      path += '404.html';
+      res.statusCode = 404;
+  
+  }
+
+  fs.readFile(path, (err,data) => {
+    if(err){
+      console.log(err);
+      res.end();
+    }
+    res.end(data);
+  });
+})
+
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
+

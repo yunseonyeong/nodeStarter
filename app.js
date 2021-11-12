5233ea69003e384d83d82c35bbe788aaac1b41ad
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-11-12 15:02:50 +0900

    feat : add next()

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ app.set('view engine', 'ejs');
 //listen for request
 app.listen(3000);
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   console.log("new request made:");
   console.log("host:", req.hostname);
   console.log("path:", req.path);
   console.log("method:", req.method);
+  
+  next();
 });
 
 // get method (url, (req, res) => {})
